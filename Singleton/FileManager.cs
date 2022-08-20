199e3912f6281cf54530ab9ac81c595c59b67619
Liusp <thodsaphonsp@gmail.com>
2022-08-20 15:29:11 +0700

    add singleton

---
+++ Singleton/FileManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public sealed class FileManager
+    {
+        public string Id { get; } = Guid.NewGuid().ToString();
+       private  FileManager()
+        {
+            
+        }
+
+       private static FileManager _fileManager;
+
+       public static FileManager GetInstance()
+       {
+           if (_fileManager == null)
+           {
+               _fileManager = new FileManager();
+           }
+
+           return _fileManager;
+       }
+
+       public string ReadFile()
+       {
+           return $"{this.Id} read file";
+       }
+
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,7 @@
+﻿
+//https://online.visual-paradigm.com/w/pplxtipa/diagrams/#diagram:workspace=pplxtipa&proj=0&id=14
+using Singleton;
+
+Console.WriteLine(FileManager.GetInstance().ReadFile());
+Console.WriteLine(FileManager.GetInstance().ReadFile());
+Console.WriteLine(FileManager.GetInstance().ReadFile());
